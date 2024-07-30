e5d2721400a02da88c3d3bf99cfcf131acf367a4
sumin <wjstnals5@naver.com>
2024-07-30 09:28:36 +0900

    다시 오류수정

--- Test.java
+++ Test.java
@@ -2,10 +2,5 @@ class Test {
 	public static void main(String[] args){
 		System.out.println("안녕하세요");
 		System.out.println("반갑습니다");
-<<<<<<< HEAD
-		System.out.println("반갑습니다");
-                System.out.println("또 만났네요");
-=======
->>>>>>> parent of fc3f42c (222)
 	}
 }	
